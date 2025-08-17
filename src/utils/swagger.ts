caf80a5ff0d01c87d6c32d74c527feb8630bf868
Samson Ude <samson_ude@yahoo.com>
2025-08-17 04:50:19 +0100

    swagger update

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -11,7 +11,8 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: `${config.host}:${config.port}/api`,
+        //url: `${config.host}:${config.port}/api`,
+        url: `${config.host}/api`,
       },
     ],
     components: {
